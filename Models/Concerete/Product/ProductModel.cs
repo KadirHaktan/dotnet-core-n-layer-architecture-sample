53b1d23fc369733350661c2af77dfe8d46a031a7
Hacktin <isues41@gmail.com>
2020-10-04 15:43:47 +0300

    concerete product model has been created

---
+++ Models/Concerete/Product/ProductModel.cs
@@ -0,0 +1,23 @@
+﻿using Core.Model;
+using System;
+using Models.Abstract.Product;
+
+namespace Models.Concerete.Product
+{
+    public class ProductModel:IProductModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Stock { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public string InnerBarcode { get; set; }
+    }
+}
