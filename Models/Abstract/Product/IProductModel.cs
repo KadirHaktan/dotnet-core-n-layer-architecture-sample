34b5f6d85ad572960ed6a4674ef42d7ec10b5172
Hacktin <isues41@gmail.com>
2020-10-04 15:43:34 +0300

    product model interface has been created

---
+++ Models/Abstract/Product/IProductModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Model;
+
+namespace Models.Abstract.Product
+{
+    public interface IProductModel:IModel
+    {
+        public int Id { get; set; }
+    }
+}
