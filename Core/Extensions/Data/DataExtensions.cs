1416ae15cc0cb5ceeaed92bc76109eac2887589c
Hacktin <isues41@gmail.com>
2020-10-04 15:41:58 +0300

    data extension has been created

---
+++ Core/Extensions/Data/DataExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Core.Entity;
+
+namespace Core.Extensions.Data
+{
+    public static class DataExtensions
+    {
+        public static IQueryable<T> IncludeMultiple<T>(this IQueryable<T> query,
+            params Expression<Func<T, object>>[] predicates)
+            where T : class, IEntity
+        {
+            if (predicates != null)
+            {
+                query = predicates.Aggregate(query, (current, include) => current.IncludeMultiple(include));
+
+            }
+
+            return query;
+        }
+    }
+}
