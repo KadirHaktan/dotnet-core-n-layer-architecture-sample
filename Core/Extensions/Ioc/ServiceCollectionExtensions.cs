39eeda1f46e897e22b666448986dbb8003e71baf
Hacktin <isues41@gmail.com>
2020-10-04 15:42:11 +0300

    Service extension has been created

---
+++ Core/Extensions/Ioc/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Ioc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Extensions.Ioc
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDependencyResolvers(this IServiceCollection services, ICoreModule[] modules)
+        {
+            foreach (var module in modules )
+            {
+                module.Load(services);
+            }
+
+            return ServiceCollectionTool.Create(services);
+        }
+    }
+}
