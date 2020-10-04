64be622a97ada8596b4dbee49323b45c02c16990
Hacktin <isues41@gmail.com>
2020-10-04 15:42:48 +0300

    base service interface has been created

---
+++ Core/Services/IService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entity;
+using Core.Model;
+using Core.Services.Response;
+
+namespace Core.Services
+{
+    public interface IService<T> where T:class,IModel
+    {
+        ServiceResponse<T> FindById(int id);
+
+        Task<ServiceResponse<T>> FindByIdAsync(int id);
+
+        ServiceResponse<T> List();
+
+        Task<ServiceResponse<T>> ListAsync();
+
+
+        ServiceResponse<int> Insert(T model);
+
+        ServiceResponse<int> Update(T model);
+
+        ServiceResponse<bool> Delete(T model);
+    }
+}
