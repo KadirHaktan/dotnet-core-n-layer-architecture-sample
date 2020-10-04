87808e0d5a4346a134333136adfe5219096e5f4c
Hacktin <isues41@gmail.com>
2020-10-04 15:44:08 +0300

    base product service interface has been created

---
+++ Service/Abstract/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Services.Response;
+using Models.Abstract.Product;
+
+namespace Service.Abstract
+{
+    public interface IProductService<T>:IService<T> where T:class,IProductModel
+    {
+    }
+}
