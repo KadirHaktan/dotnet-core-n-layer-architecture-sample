c5ca51ab35d65e7e9d1dcabadb69fc5a36b880c7
Hacktin <isues41@gmail.com>
2020-10-04 15:39:00 +0300

    db context has been created

---
+++ Data/EF/SampleAppContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Data.EF
+{
+    public class SampleAppContext:DbContext
+    {
+        public SampleAppContext()
+        {
+            
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+
+        public SampleAppContext(DbContextOptions<SampleAppContext> options):base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-0KLVU2P\SQLEXPRESS;Initial Catalog=SampleProductDB;Integrated Security=True");
+        }
+    }
+}
