39beba1bf12edb9ae9c721ca55ce194b813f732e
Hacktin <isues41@gmail.com>
2020-10-04 15:40:44 +0300

    Db table entities have been created

---
+++ Entities/Category.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Entities
+{
+    public class Category:IEntity
+    {
+        public Category()
+        {
+            Products=new Collection<Product>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entity;
+
+namespace Entities
+{
+    public class Product:IEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Stock { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public string InnerBarcode { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
