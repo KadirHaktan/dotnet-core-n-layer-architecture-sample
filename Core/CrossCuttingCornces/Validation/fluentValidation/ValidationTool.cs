ce7ad594c4315de3a08978e49fa743a423e57050
Hacktin <isues41@gmail.com>
2020-10-07 16:02:32 +0300

    validation context's type has been edited

--- Core/CrossCuttingCornces/Validation/fluentValidation/ValidationTool.cs
+++ Core/CrossCuttingCornces/Validation/fluentValidation/ValidationTool.cs
@@ -6,11 +6,11 @@ using FluentValidation;
 
 namespace Core.CrossCuttingCornces.Validation.fluentValidation
 {
-    public static class ValidationTool<T> where T:class,IEntity
+    public static class ValidationTool
     {
-        public static void Validate(IValidator validator, T entity)
+        public static void Validate(IValidator validator, object entity)
         {
-            var context = new ValidationContext<T>(entity);
+            var context = new ValidationContext<object>(entity);
 
             var result = validator.Validate(context);
 
