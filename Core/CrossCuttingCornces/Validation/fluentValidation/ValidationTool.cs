c6a9813fe3675e856a867608dc6fd277ef0f29c6
Hacktin <isues41@gmail.com>
2020-10-07 15:52:02 +0300

    fluent validation's tool has been created

---
+++ Core/CrossCuttingCornces/Validation/fluentValidation/ValidationTool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entity;
+using FluentValidation;
+
+namespace Core.CrossCuttingCornces.Validation.fluentValidation
+{
+    public static class ValidationTool<T> where T:class,IEntity
+    {
+        public static void Validate(IValidator validator, T entity)
+        {
+            var context = new ValidationContext<T>(entity);
+
+            var result = validator.Validate(context);
+
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
