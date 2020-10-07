8d00eb20e0d74929098fecd75bc5cc0f8afec02a
Hacktin <isues41@gmail.com>
2020-10-07 15:52:33 +0300

    aspect selector's dependency has been created

---
+++ Service/DependencyResolvers/AutofacServiceModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Castle.DynamicProxy;
+using Core.Aspects.Base;
+using Module = Autofac.Module;
+
+namespace Service.DependencyResolvers
+{
+    public class AutofacServiceModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var assemblies = Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assemblies)
+                .AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
+        }
+    }
+}
