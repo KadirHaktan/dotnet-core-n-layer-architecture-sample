b284e11b0e2be4fe48145469faef5e6bd50cac75
Hacktin <isues41@gmail.com>
2020-10-08 18:37:37 +0300

    cache manager interface has been created

---
+++ Core/CrossCuttingCornces/Caching/ICacheManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingCornces.Caching
+{
+    public interface ICacheManager
+    {
+        T Get<T>(string key);
+
+        object Get(string key);
+
+
+        void Add(string key, object data, int duration);
+
+        bool IsAdd(string key);
+
+        void Remove(string key);
+
+        void RemoveByPattern(string key);
+    }
+}
