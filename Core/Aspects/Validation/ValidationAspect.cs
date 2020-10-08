68ccb2c19d343c3c6c0c1b07d223a21b2f1432b5
Hacktin <isues41@gmail.com>
2020-10-08 18:37:21 +0300

    aspect class's name has been changed

---
+++ Core/Aspects/Validation/ValidationAspect.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.DynamicProxy;
+using Core.Aspects.Base;
+using Core.CrossCuttingCornces.Validation.fluentValidation;
+using FluentValidation;
+
+namespace Core.Aspects.Validation
+{
+    public class ValidationAspect:MethodInterceptors
+    {
+        public Type Type;
+
+
+        public ValidationAspect(Type type)
+        {
+            if (!typeof(IValidator).IsAssignableFrom(type))
+            {
+                throw new Exception();
+            }
+
+            this.Type = type;
+        }
+
+        public override void OnBefore(IInvocation invocation)
+        {
+            var validator = (IValidator) Activator.CreateInstance(Type);
+            var entityType = Type.BaseType.GetGenericArguments()[0];
+
+            var entities = invocation.Arguments.Where(e => e.GetType() == entityType);
+
+            foreach (var entity in entities)
+            {
+                ValidationTool.Validate(validator,entity);
+            }
+        }
+    }
+}
