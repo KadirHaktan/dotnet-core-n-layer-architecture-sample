c2cd94f25a0d115be851c9a3b984976bbb7beaeb
Hacktin <isues41@gmail.com>
2020-10-08 18:38:13 +0300

    product model's validation rules have been defined

---
+++ Validations/Product/ProductModelValidation.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Models.Concerete.Product;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Validations.Product
+{
+    public class ProductModelValidation:AbstractValidator<ProductModel>
+    {
+        public ProductModelValidation()
+        {
+            RuleFor(p => p.Price).Must(p => p > 0);
+            RuleFor(p => p.Name).NotNull();
+            RuleFor(p => p.InnerBarcode).MinimumLength(5);
+        }
+    }
+}
