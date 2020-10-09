4f9baac77f8205b106eb9a96164cc02ffc870ec9
Hacktin <isues41@gmail.com>
2020-10-09 11:38:55 +0300

    exception handler middleware extension has been created

---
+++ Core/Extensions/Middleware/ExceptionHandlerMiddlewareExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Handlers.Error;
+using Microsoft.AspNetCore.Builder;
+
+namespace Core.Extensions.Middleware
+{
+    public static class ExceptionHandlerMiddlewareExtension
+    {
+        public static void ConfigureToCustomExceptionMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionHandlerMiddleware>();
+        }
+    }
+}
