8c591146be424c35c27b84459fee6bbd0cc8b908
Hacktin <isues41@gmail.com>
2020-10-10 11:22:25 +0300

    unit of work dependency has been handled

--- Service/DependencyResolvers/ServiceModule.cs
+++ Service/DependencyResolvers/ServiceModule.cs
@@ -5,8 +5,10 @@ using Caching.redis;
 using Core.CrossCuttingCornces.Caching;
 using Core.Ioc;
 using Core.Repository;
+using Core.UnitOfWork;
 using Data.EF;
 using Data.EF.Repository;
+using Data.EF.UnitOfWork;
 using Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -21,9 +23,11 @@ namespace Service.DependencyResolvers
         public void Load(IServiceCollection services)
         {
             services.AddScoped<IRepository<Product>, EfGenericRepositoryBase<Product,SampleAppContext>>();
+            services.AddScoped<IUnitOfWork,UnitOfWork>();
+
             services.AddTransient<IProductService<ProductModel>, ProductModelService>();
 
-            services.AddDbContext<SampleAppContext>(options => options.UseSqlServer(""));
+            services.AddDbContext<SampleAppContext>(options => options.UseSqlServer(@"Data Source=DESKTOP-0KLVU2P\SQLEXPRESS;Initial Catalog=SampleProductDB;Integrated Security=True"));
 
             services.AddSingleton<RedisServer>();
             services.AddSingleton<ICacheManager, RedisCacheManager>();
