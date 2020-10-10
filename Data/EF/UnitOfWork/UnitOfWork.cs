49b1a7da67f7f13c7d5b5616761fbec65cb427f3
Hacktin <isues41@gmail.com>
2020-10-10 11:19:17 +0300

    ef's unit of work class has been created

---
+++ Data/EF/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Core.UnitOfWork;
+
+namespace Data.EF.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly SampleAppContext _context;
+
+        public UnitOfWork(SampleAppContext context)
+        {
+            this._context = context;
+        }
+        public int Commit()
+        {
+            return _context.SaveChanges();
+        }
+
+        public async Task<int> CommitAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
