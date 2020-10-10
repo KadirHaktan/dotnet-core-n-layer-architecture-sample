fef4b767a80e0d224d9c864fa9fdabf7a4d5d382
Hacktin <isues41@gmail.com>
2020-10-10 11:22:48 +0300

    unit of work interface has been created

---
+++ Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entity;
+using Core.Repository;
+
+namespace Core.UnitOfWork
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        Task<int> CommitAsync();
+
+        int Commit();
+    }
+}
